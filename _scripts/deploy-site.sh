ae6f9010e1828c1b3bcca0413e940a27a6e50921
Lorenzo Flueckiger <Lorenzo.Flueckiger@gmail.com>
2020-08-22 22:27:24 -0700

    added actual simple script to deploy the website

--- _scripts/deploy-site.sh
+++ _scripts/deploy-site.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-today=`date +%Y-%m-%d_%H-%M-%S`
+scriptdir=$(dirname $0)
+webdir=/var/www/plume
 
-echo "${today}" >> test.log
+/usr/bin/git pull
 
+cd ${scriptdir}/..
+
+bundler exec jekyll build
+ret=$?
+
+if [ $ret -ne 0 ] ; then
+  echo "Jekyll build failed :-("
+  exit -1
+fi
+
+rsync --archive --delete _site/ ${webdir}
 
